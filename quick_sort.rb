d82c9e4c44585fc96b3801109b4b51bba9acef81
Peter Prakobkit <kprakobkit@gmail.com>
2014-12-13 16:03:43 -0800

    implement quicksort

--- quick_sort.rb
+++ quick_sort.rb
@@ -1,27 +1,27 @@
 def quick_sort(array)
-  p array
-  return array if (array.size == 0 || array.size == 1)
+  return array if array.size <= 1
   pivot_index = array.size / 2
   sorted_array = quick_sort_helper(array, pivot_index)
-  array1 = sorted_array[0..pivot_index]
-  array2 = sorted_array[pivot_index+1..array.length-1]
-  p "a1 is #{array1}"
-  p "a2 is #{array2}"
+  array1 = sorted_array[0..pivot_index-1]
+  array2 = sorted_array[pivot_index..array.length-1]
   quick_sort(array1) + quick_sort(array2)
 end
 
 def quick_sort_helper(array, pivot_index)
-  array1 = []
-  array2 = []
-  pivot_array = []
+  lesser = []
+  greater = []
+  pivot = []
   array.each do |e|
     if e > array[pivot_index]
-      array2 << e
+      greater << e
     elsif e < array[pivot_index]
-      array1 << e
+      lesser << e
     else
-      pivot_index << e
+      pivot << e
     end
   end
-  array1 + pivot_array + array2
+  lesser + pivot + greater
 end
+
+p quick_sort([1,21,55,6,33,100])
+p quick_sort([1,1000,90,1002,100,90,500,100])
