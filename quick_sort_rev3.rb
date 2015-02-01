d7cabe36f70746a39a61e3b3bf80c7798df10781
Stefano Schiavi <stefano@bvprojects.org>
2015-02-01 20:38:07 +0100

    add pure in place quick sort method

---
+++ quick_sort_rev3.rb
@@ -0,0 +1,20 @@
+def quick_sort(array, l_bound=0, u_bound=array.size-1)
+  return array if u_bound <= l_bound+1 || l_bound+1 >= u_bound
+  pivot_index = l_bound
+  pivot_val = array[pivot_index]
+  low_up_bound_index = l_bound+1
+
+  (l_bound+1).upto(u_bound) do |index|
+    if array[index] <= pivot_val
+      array[low_up_bound_index], array[index] = array[index], array[low_up_bound_index]
+      low_up_bound_index += 1
+    end
+  end
+  array[pivot_index], array[low_up_bound_index-1] = array[low_up_bound_index-1], array[pivot_index]
+
+  array = quick_sort(array, low_up_bound_index, u_bound)
+  quick_sort(array, l_bound, low_up_bound_index-2)
+end
+
+p quick_sort([1,21,55,6,33,100]) == [1, 6, 21, 33, 55, 100]
+p quick_sort([1,1000,90,1002,100,90,500,100]) == [1, 90, 90, 100, 100, 500, 1000, 1002]
